82a316cb6539faa0262313575365cab96b043243
matucking1305 <guilherme130501@gmail.com>
2022-11-15 22:07:34 -0300

    Adicionando data

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/matucking1305/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/matucking1305/js-developer-portfolio/master/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
